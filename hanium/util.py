8c67901b923ebdac431ed1841858c44354eea65e
juyub <126839881+juyub@users.noreply.github.com>
2023-10-27 16:33:53 +0900

    Delete hanium directory

--- hanium/util.py
+++
@@ -1,6 +0,0 @@
-import cx_Oracle
-
-def get_connection():
-    dsn = cx_Oracle.makedsn("localhost", "1521", service_name="xe")
-    connection = cx_Oracle.connect(user="c##hr2", password="1234", dsn=dsn)
-    return connection
